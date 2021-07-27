cacabd249f65e21f19c2c75b39f5ef3cfe34a87c
Martin <martinmdvld@gmail.com>
2021-07-27 15:58:38 +0200

    test

--- resources/views/partial/bloc-presentation.blade.php
+++ resources/views/partial/bloc-presentation.blade.php
@@ -1,7 +1,7 @@
 
 <div style="height: 850px" class="d-flex justify-content-center align-items-center">
     <div  class="bloc-presentation d-flex flex-column align-items-center">
-        <img src="{{asset('image/Logo-martin-site.png')}}" class="reveal-2" style="width: 50%" alt="">
+        <img src="{{secure_asset('image/Logo-martin-site.png')}}" class="reveal-2" style="width: 50%" alt="">
         <p class="p-2 titre1 text-center reveal-3">Développeur Web</p>
         <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
             @foreach (LaravelLocalization::getSupportedLocales() as $localeCode => $properties)
